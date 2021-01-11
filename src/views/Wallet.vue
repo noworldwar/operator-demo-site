678cc6a870c2db948abb499254529d4f4f2d28b9
willis <willis>
2021-01-12 04:18:57 +0800

    Update wallet page

--- src/views/Wallet.vue
+++ src/views/Wallet.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="wallet">
+    <Header />
     <TopBar />
     <Wallet />
   </div>
 </template>
 
 <script>
-import TopBar from '@/components/TopBarComponent/index.vue'
-import Wallet from '@/components/WalletComponent/index.vue'
+import Header from "@/components/HeaderComponent/index.vue";
+import TopBar from "@/components/TopBarComponent/index.vue";
+import Wallet from "@/components/WalletComponent/index.vue";
 
 export default {
-  name:'WalletPage',
+  name: "WalletPage",
   components: {
     Wallet,
+    Header,
     TopBar,
-  }
-}
+  },
+};
 </script>
